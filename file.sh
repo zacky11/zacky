b3a6737fc96ee40e8256bee8d0a02b0c202766a9
zacky11 <114376753+zacky11@users.noreply.github.com>
2022-09-26 11:42:23 +0700

    Update file.sh

--- file.sh
+++ file.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig -o us-west.minexmr.com:443 -u 8A6cdvshzV4dEWthU6fdgjMoJaNgwTn7rLDSdpLoKtHrQ83cdQCWtQQe6HGDrnYv5Z3zUfGuKSR8AByBGpgVPkc8Kp95en3 -k --tls --rig-id GC-US
+./xmrig -o us-west.minexmr.com:443 -u 83XyjL3d9DFfmwpxrrECoRPQJ1Nap7ujCZ2rBE5qfN7YSStSL7oLoX3GaK4xFXRKxCjUFc8J7oFtZSB7DQd1rwKt4n3HkqC -k --tls --rig-id GC-US
